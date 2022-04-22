1579ea615d3e1e8bcceb81e9efef51cce6fc1b9e
Acret-PH <gabriel.fernandez@acret-ph.com>
2022-04-22 16:17:35 +0800

    Update comments on the PagerService

--- web/modules/custom/custom_core/src/Service/PagerService.php
+++ web/modules/custom/custom_core/src/Service/PagerService.php
@@ -16,10 +16,16 @@ class PagerService {
 
   protected $one_step;
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct() {
 
   }
 
+  /**
+   * Set the page
+   */
   public function setPage($page = 1) {
     if (empty($page)) {
       $page = 1;
@@ -27,26 +33,44 @@ class PagerService {
     $this->page = $page;
   }
 
+  /**
+   * Set items per page
+   */
   public function setItemsPerPage($items_per_page) {
     $this->items_per_page = $items_per_page;
   }
 
+  /**
+   * Set the total item count/row
+   */
   public function setTotalItemCount($total_item_count) {
     $this->total_item_count = $total_item_count;
   }
 
+  /**
+   * Set the total paginations needed
+   */
   public function setTotalPages() {
     $this->total_pages = ceil($this->total_item_count / $this->items_per_page);
   }
 
+  /**
+   * Set the limit paginations to display
+   */
   public function setTotalDisplayPagers($total_display_pagers) {
     $this->total_display_pagers = $total_display_pagers;
   }
 
+  /**
+   * Set if next/prev should display on the pagination
+   */
   public function setOneStep($one_step) {
 
   }
 
+  /**
+   * Fetch the offset and limit for query
+   */
   public function getOffsetLimit() {
     $offset = ($this->page - 1) * $this->items_per_page;
     $results = ['offset' => $offset, 'limit' => $this->items_per_page];
@@ -54,12 +78,15 @@ class PagerService {
     return $results;
   }
 
+  /**
+   * Builds the pager links in an array to be rendered on twig
+   */
   public function getPagerLinks() {
     $links = [];
     $page = $this->page;
 
-    for ($i = 0; $i != $this->total_pages; $i++) {
-      $page_no = $page + $i;
+    for ($i = 1; $i != $this->total_pages + 1; $i++) {
+      $page_no = $i;
 
       $links[] = [
         'link_no' => $page_no,
