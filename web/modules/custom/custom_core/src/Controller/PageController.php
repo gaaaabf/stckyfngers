8e68e272859f376add6079891c9a04e052207b2d
Acret-PH <gabriel.fernandez@acret-ph.com>
2022-04-22 10:01:40 +0800

    Created UserModel Service

--- web/modules/custom/custom_core/src/Controller/PageController.php
+++ web/modules/custom/custom_core/src/Controller/PageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManager;
-use Drupal\custom_core\UserModel;
+use Drupal\custom_core\Service\UserModel;
 
 /**
  * Class PageController.
@@ -24,10 +24,10 @@ class PageController extends ControllerBase {
   /**
    * {@inheritdoc}
    */
-  public function __construct(Connection $database, EntityTypeManager $entity_type_manager) {
+  public function __construct(Connection $database, EntityTypeManager $entity_type_manager, UserModel $user_model) {
     $this->database = $database;
     $this->entity_type_manager = $entity_type_manager;
-    $this->user_model = new UserModel($database, $entity_type_manager);
+    $this->user_model = $user_model;
   }
 
   /**
@@ -37,6 +37,7 @@ class PageController extends ControllerBase {
     return new static(
       $container->get('database'),
       $container->get('entity_type.manager'),
+      $container->get('custom_core.user_model'),
     );
   }
 
@@ -58,7 +59,7 @@ class PageController extends ControllerBase {
    */
   public function artistsPage(Request $request) {
 
-    \Kint::dump($this->user_model);
+    $this->user_model->fetchArtists();
 
     $items = ['artist1', 'artist2', 'artist3'];
 
